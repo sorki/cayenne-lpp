4038e6af5e443ffd67cfa5394e713b0622046d73
Richard Marko <srk@48.io>
2021-03-20 10:58:44 +0100

    base16-bytestring >= 1

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -38,14 +38,14 @@ spec :: Spec
 spec = do
   it "parses samples" $ do
     mapM_ (\x -> (decode . encode) x `shouldBe` x) testCases
-    (decode . fst $ B16L.decode "018806765ff2960a0003e8") `shouldBe` (1, GPS 42.3519 (-87.9094) 10.0)
-    (decode . fst $ B16L.decode "067104D2FB2E0000")       `shouldBe` (6, Accelerometer 1.234 (-1.234) 0.0)
-    (decode . fst $ B16L.decode "0167FFD7")               `shouldBe` (1, Temperature (-4.1))
-    (decode . fst $ B16L.decode "03670110056700FF")       `shouldBe` (3, Temperature 27.2)
+    (decode <$> B16L.decode "018806765ff2960a0003e8") `shouldBe` Right (1, GPS 42.3519 (-87.9094) 10.0)
+    (decode <$> B16L.decode "067104D2FB2E0000")       `shouldBe` Right (6, Accelerometer 1.234 (-1.234) 0.0)
+    (decode <$> B16L.decode "0167FFD7")               `shouldBe` Right (1, Temperature (-4.1))
+    (decode <$> B16L.decode "03670110056700FF")       `shouldBe` Right (3, Temperature 27.2)
 
-    (decodeMany . fst $ B16L.decode "018806765ff2960a0003e8067104D2FB2E00000167FFD703670110056700FF")
+    (decodeMany <$> B16L.decode "018806765ff2960a0003e8067104D2FB2E00000167FFD703670110056700FF")
       `shouldBe`
-      [(1, GPS 42.3519 (-87.9094) 10.0), (6, Accelerometer 1.234 (-1.234) 0.0), (1,Temperature (-4.1)), (3,Temperature 27.2), (5,Temperature 25.5)]
+      Right [(1, GPS 42.3519 (-87.9094) 10.0), (6, Accelerometer 1.234 (-1.234) 0.0), (1,Temperature (-4.1)), (3,Temperature 27.2), (5,Temperature 25.5)]
 
 main :: IO ()
 main = hspec spec
